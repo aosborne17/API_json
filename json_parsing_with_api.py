4f7651797e0ea9c1b795976c05297c2646dcc48a
aosborne17 <AOsborne@spartaglobal.com>
2020-07-07 14:59:12 +0100

    'json_and_requests'

--- json_parsing_with_api.py
+++ json_parsing_with_api.py
@@ -7,7 +7,19 @@ print(post_codes_req.status_code)
 
 # Why should we use built in packages
 
-if post_codes_req.status_code == 200:
-    print("Success!")
-elif post_codes_req.status_code == 400:
-    print("Page Not Available")
+"""
+Creating this class allows us to recall this functionality
+whenever we need to as well as importing it to other files
+"""
+class Live_Web_Status_Code:
+
+    def check_status_code(self):
+        if post_codes_req.status_code == 200:
+            return "Success"
+        elif post_codes_req.status_code == 400:
+            return "Page Not Available"
+
+
+requests1 = Live_Web_Status_Code()
+
+print(requests1.check_status_code())
