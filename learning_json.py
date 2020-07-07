fa67f1ab86d42fd17fd306e415525f502dd0b1ac
aosborne17 <AOsborne@spartaglobal.com>
2020-07-07 10:56:39 +0100

    'json_encoding_and_decoding'

--- learning_json.py
+++ learning_json.py
@@ -1,29 +1,3 @@
-# What is JSON?
-## Javascript Object Notation 
-
-### json is a syntax for exchanging data between a browser and server
-
-- json is also used to parse data from existing files or web files
-- parsing is where when given a sequence of text, identifying the various parts of the text and is able to assign ‘meaning’ to the text
-- The data can only be text, thus json is written in json format
-``` bash
-- Data Types
-a string
-a number
-an object (json object)
-an array
-boolean
-null
-
-```
-In json, data is stored in name/value pairs:
-
-``` bash
-{"name": "Andrew", "age": "20"}
-- The pairing is seperated by a comma
-```
-
-```python
 import json
 
 car_data = {"name": "Tesla", "engine": "electric"}
@@ -68,4 +42,3 @@ had just made 'new_json_file'
 
 Using the 'load' method allowed us to read over the file
 """
-```
